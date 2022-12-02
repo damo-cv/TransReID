b0f6cdca1ac298d7b50552d0c3e9bf3844176dc1
chachao <18303916@qq.com>
2022-12-02 20:10:15 +0800

    partselect + knn

--- model/backbones/vit_pytorch.py
+++ model/backbones/vit_pytorch.py
@@ -135,6 +135,40 @@ class Mlp(nn.Module):
         x = self.drop(x)
         return x
 
+class kNNAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.,topk=90):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.topk = topk
+
+    def forward(self, x, islast):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        if(islast == False):
+            # the core code block
+            mask=torch.zeros(B,self.num_heads,N,N,device=x.device,requires_grad=False)
+            index=torch.topk(attn,k=self.topk,dim=-1,largest=True)[1]
+            mask.scatter_(-1,index,1.)
+            attn=torch.where(mask>0,attn,torch.full_like(attn,float('-inf')))
+            # end of the core code block
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x, attn
 
 class Attention(nn.Module):
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
@@ -170,16 +204,16 @@ class Block(nn.Module):
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.attn = kNNAttention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop, topk=100)
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-    def forward(self, x):
-        atte_output, weight = self.attn(self.norm1(x))
+    def forward(self, x, islast):
+        atte_output, weight = self.attn(self.norm1(x), islast)
         x = x + self.drop_path(atte_output)
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x, weight
@@ -413,7 +447,7 @@ class TransReID(nn.Module):
         else:
             attn_weights = []
             for blk in self.blocks[:-1]:
-                x, weights = blk(x)
+                x, weights = blk(x, False)
                 attn_weights.append(weights)
             part_num, part_inx = self.part_select(attn_weights)
             part_inx = part_inx + 1
@@ -424,7 +458,8 @@ class TransReID(nn.Module):
             parts = torch.stack(parts).squeeze(1)
             concat = torch.cat((x[:, 0].unsqueeze(1), parts), dim=1)
             last_blk = self.blocks[-1]
-            last_x, last_weights = last_blk(concat)
+            islast = True
+            last_x, last_weights = last_blk(concat, islast)
             last_encoded = self.norm(last_x)
 
             return last_encoded[:, 0]
